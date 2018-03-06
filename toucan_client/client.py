a14ded5d621a16408789533063bf81c08d74c0aa
fspot <fred@fspot.org>
2018-03-06 18:01:38 +0100

    simplify client

--- toucan_client/client.py
+++ toucan_client/client.py
@@ -1,16 +1,9 @@
-import logging
-
 import requests
-
-logger = logging.getLogger(__name__)
 
 
 class ToucanClient:
     """
-    Small client for sending request to a Toucan Toco back end.
-    The constructor's mandatory parameter is the API base url. One can pass
-    a small app name or a list of small app names, a token or an auth object
-    for authentication.
+    Small client for sending requests to a Toucan Toco backend.
 
     >>> # Example: Fetch etl config
     >>> client = ToucanClient('https://api.some.project.com')
@@ -22,53 +15,21 @@ class ToucanClient:
     >>> # response.status_code equals 200 if everything went well
     """
 
-    EXTRACTION_CACHE_PATH = 'extraction_cache'
+    def __init__(self, base_route, _path=None, **kwargs):
+        self._base_route = base_route.strip().rstrip('/')
+        self._requests_kwargs = kwargs
+        self._path = tuple(_path) if _path else ()
 
-    def __init__(self, base_route, **kwargs):
-        # type: (str) -> SmallAppRequester
-        self.__dict__['_path'] = []
-        self.__dict__['kwargs'] = kwargs
-        self.__dict__['stage'] = ''
-        self.__dict__['_dfs'] = None
-        self.__dict__['_cache_path'] = None
-
-        self.__dict__['base_route'] = base_route
-        if base_route.endswith('/'):
-            self.__dict__['base_route'] = base_route[:-1]
-
-    @property
-    def method(self):
-        # type: () -> str
-        return self._path[-1]
-
-    @property
-    def options(self):
-        # type: () -> str
-        if self.stage:
-            return '?stage={}'.format(self.stage)
-        return ''
-
-    @property
-    def route(self):
-        # type: () -> str
-        route = '/'.join([self.base_route] + self._path[:-1])
-        route += self.options
-
-        self.__dict__['_path'] = []
-        return route
+    def __getitem__(self, key):
+        new_path = self._path + (key,)
+        return ToucanClient(self._base_route, new_path, **self._requests_kwargs)
 
     def __getattr__(self, key):
-        self._path.append(key)
-        return self
+        return self[key]  # forward to __getitem__
 
-    def __setattr__(self, key, value):
-        if key == 'stage':
-            self.__dict__['stage'] = value
-        else:
-            self.kwargs[key] = value
-
-    def __call__(self):
-        # type: () -> requests.Response
-        method, route = self.method, self.route
-        func = getattr(requests, method)
-        return func(route, **self.kwargs)
+    def __call__(self, **kwargs):
+        method = self._path[-1]
+        method_func = getattr(requests, method)
+        url = '/'.join((self._base_route,) + self._path[:-1])
+        requests_kwargs = {**self._requests_kwargs, **kwargs}
+        return method_func(url, **requests_kwargs)
