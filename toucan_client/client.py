50b633b05a8eadd68fe6da0d0e047a209acc8675
Eric Jolibois <em.jolibois@gmail.com>
2022-02-11 14:39:52 +0100

    style: black format

--- toucan_client/client.py
+++ toucan_client/client.py
@@ -8,8 +8,8 @@ def build_requests_kwargs(kwargs, extra_kwargs):
     requests_kwargs.update(extra_kwargs)
 
     if "stage" in requests_kwargs:
-        requests_kwargs['params'] = requests_kwargs.get('params', {})
-        requests_kwargs['params'].update({'stage': requests_kwargs.pop('stage')})
+        requests_kwargs["params"] = requests_kwargs.get("params", {})
+        requests_kwargs["params"].update({"stage": requests_kwargs.pop("stage")})
 
     return requests_kwargs
 
@@ -29,7 +29,7 @@ class ToucanClient:
     """
 
     def __init__(self, base_route, _path=None, **kwargs):
-        self._base_route = base_route.strip().rstrip('/')
+        self._base_route = base_route.strip().rstrip("/")
         self._requests_kwargs = kwargs
         self._path = tuple(_path) if _path else ()
 
@@ -43,6 +43,6 @@ class ToucanClient:
     def __call__(self, **kwargs):
         method = self._path[-1]
         method_func = getattr(requests, method)
-        url = '/'.join((self._base_route,) + self._path[:-1])
+        url = "/".join((self._base_route,) + self._path[:-1])
         requests_kwargs = build_requests_kwargs(self._requests_kwargs, kwargs)
         return method_func(url, **requests_kwargs)
